3ae5a01f4f5c09ff5c1bf0c2a53c1aa96389761d
yevhen <yevhenlebid51@gmail.com>
2024-03-16 01:14:18 +0200

    TM-2: Implement user auth and create roles for admin and customer

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use App\Observers\UserObserver;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -22,10 +24,12 @@ class EventServiceProvider extends ServiceProvider
 
     /**
      * Register any events for your application.
+     *
+     * @return void
      */
-    public function boot(): void
+    public function boot()
     {
-        //
+        User::observe(UserObserver::class);
     }
 
     /**
